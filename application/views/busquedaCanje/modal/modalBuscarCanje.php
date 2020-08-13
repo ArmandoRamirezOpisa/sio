f5716366b7cd2f630130c59c7dc2ff657a9822b3
Armando Ramirez <armando.ramirez@opisa.com>
2020-08-13 11:35:37 -0500

    cambiar el modal a target_blank

--- application/views/busquedaCanje/modal/modalBuscarCanje.php
+++ application/views/busquedaCanje/modal/modalBuscarCanje.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="moreInfo">Info Canje</h5>
+                    <h5 class="modal-title" id="moreInfo">Info Canje - Cambiar pagina normal</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
