42bf2038a9fa9827f046815a91f502745da743c3
Armando Ramirez <armando.ramirez@opisa.com>
2020-08-24 12:37:01 -0500

    Tabla de mensajerias esta lista

--- application/views/busquedaCanje/mesajerias_view.php
+++ application/views/busquedaCanje/mesajerias_view.php
@@ -1,7 +1,7 @@
 						<?php if($mensajerias){ ?>
 						<select class="form-control" id="mensajeria">
 							<?php foreach($mensajerias as $mensajeria){ ?>
-							<option><?php echo $mensajeria['mensajeria']; ?></option>
+							<option value="<?php echo $mensajeria['idMensajeria']; ?>"><?php echo $mensajeria['mensajeria']; ?></option>
 							<?php } ?>
     					</select>
 						<?php } ?>
